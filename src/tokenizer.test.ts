ecf1cb2d455d4f00e9311a4919540ee429c79b55
Johan Björk <johanimon@gmail.com>
2024-05-29 00:24:21 +0200

    Add very helpful tests

--- src/tokenizer.test.ts
+++ src/tokenizer.test.ts
@@ -1,6 +1,20 @@
 import { describe, it, expect } from "bun:test";
+import { Tokenizer, type KnownToken, type KnownTokenType } from "./tokenizer";
 
 describe("tokenizer", () => {
-  it("should tokenize years", () => {
+  const cases: Array<[string, Array<KnownTokenType>]> = [
+    ['y', ['YEAR']],
+    ['Y', ['YEAR']],
+    ['y-M-d', ['YEAR', 'DATE_SEP', 'MONTH', 'DATE_SEP', 'DAY']],
+  ];
+
+  it.each(cases)('should tokenize single tokens', (source, expected) => {
+    const types = Tokenizer.tokenize(source)
+      .map(t => t.type);
+    expect(types).toEqual(expected);
   });
+
+  it('should throw when parsing unknown tokens', () => {
+    expect(() => Tokenizer.tokenize('고양이')).toThrow();
+  })
 });
